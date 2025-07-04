4795a6345727e960e07f8375208ecee9e171d3db
Panthari Panthong <panthari.panthong@gmail.com>
2025-07-04 12:26:18 +0200

    resize pdf

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 async function downloadPDF() {
   const { jsPDF } = window.jspdf;
-
-  const button = document.querySelector(".no-print");
-  button.style.display = "none";
-
   const element = document.getElementById("form-container");
+
+  // Hide the button in PDF
+  document.querySelector(".no-print").style.display = "none";
+
   const canvas = await html2canvas(element, {
     scale: 2,
-    ignoreElements: (el) => el.classList.contains("no-print"),
+    scrollY: -window.scrollY,
   });
 
   const imgData = canvas.toDataURL("image/png");
   const pdf = new jsPDF("p", "pt", "a4");
   const pageWidth = pdf.internal.pageSize.getWidth();
   const pageHeight = pdf.internal.pageSize.getHeight();
-  const imgProps = pdf.getImageProperties(imgData);
-  const imgHeight = (imgProps.height * pageWidth) / imgProps.width;
 
-  if (imgHeight <= pageHeight) {
-    pdf.addImage(imgData, "PNG", 0, 0, pageWidth, imgHeight);
-  } else {
-    let heightLeft = imgHeight;
-    let position = 0;
+  const imgWidth = pageWidth;
+  const imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-    while (heightLeft > 0) {
-      pdf.addImage(imgData, "PNG", 0, position, pageWidth, imgHeight);
-      heightLeft -= pageHeight;
-      if (heightLeft > 0) {
-        pdf.addPage();
-        position = 0;
-      }
+  let position = 0;
+  let heightLeft = imgHeight;
+
+  // Split into multiple pages
+  while (heightLeft > 0) {
+    pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+    if (heightLeft > 0) {
+      pdf.addPage();
+      position = position - pageHeight;
     }
   }
 
   pdf.save("werkplekinspectie.pdf");
-  button.style.display = "block";
+
+  // Restore button
+  document.querySelector(".no-print").style.display = "block";
 }
